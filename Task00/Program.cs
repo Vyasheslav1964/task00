0f7642ad2fb02d3d06ed0b1dd08f778972ec0ce2
Vyasheslav1964 <137889188+Vyasheslav1964@users.noreply.github.com>
2023-07-07 14:10:20 +0300

    Добавили файлы

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.InteropServices;
+
 Console.WriteLine("Hello, World!");
+//1 Напишите программу, которая навход принимает
+//число a 
+//2 выдаёт его квадрат
+Console.WriteLine("Ведите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+int square = number*number;
+Console.WriteLine($"Квадрат числа {number} = {square}");
+
