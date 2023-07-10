80aa90d9e84fddcbf1033831eacaa00106f8551b
Vyasheslav1964 <137889188+Vyasheslav1964@users.noreply.github.com>
2023-07-10 08:48:34 +0300

    Added folder Task07

---
+++ Task07/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 0)
+{
+    number *= -1;
+}
+if (number >= 100 && number <= 999)
+{
+int lastDijit = number % 10;
+Console.Write($"Последняя цифра числа : {lastDijit}");
+}
+else
+{
+    Console.Write("Ошибка ввода");
+}
