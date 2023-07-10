1b944255a0d10c294a758f59127e17c1e1b618e6
Vyasheslav1964 <137889188+Vyasheslav1964@users.noreply.github.com>
2023-07-10 08:47:34 +0300

    Added folder Task07

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ведите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a == b * b)
+{
+    Console.Write("Yes");
+}
+else
+{
+    Console.Write("no");
+}
