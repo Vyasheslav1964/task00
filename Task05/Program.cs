619570bf0072ea02bb19d2f462272264df298885
Vyasheslav1964 <137889188+Vyasheslav1964@users.noreply.github.com>
2023-07-10 08:58:14 +0300

    Added folder Task05

---
+++ Task05/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите положительное число");
+int number = Convert.ToInt32(Console.ReadLine());
+ if(number <= 0)
+ {
+    Console.WriteLine("Ошибка ввода");
+ }
+else
+{
+    int counter = -number;
+    while(counter <= number)
+    {
+        Console.Write(counter + " "); 
+        counter = counter + 1;
+    }
+}
