548964df463ea563be75261fd10229b9f980094d
Vyasheslav1964 <137889188+Vyasheslav1964@users.noreply.github.com>
2023-07-10 08:59:18 +0300

    Added folder Task03

---
+++ Task03/Program.cs
@@ -0,0 +1,69 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите номер дня недели");
+
+string number = Console.ReadLine();
+
+/*if (number == "1")
+{
+    Console.WriteLine("Понедельник");
+}
+else if (number == "2")
+{
+    Console.WriteLine("Вторник");
+}
+else if (number == "3")
+{
+    Console.WriteLine("Среда");
+}
+else if (number == "4")
+{
+    Console.WriteLine("Четверг");
+}
+else if (number == "5")
+{
+    Console.WriteLine("Пятница");
+}
+else if (number == "6")
+{
+    Console.WriteLine("Суббота");
+}
+else if (number == "7")
+{
+    Console.WriteLine("Воскресенье");
+}
+else
+{
+  Console.WriteLine("Неверное число");
+}*/
+switch (number)
+{
+    case "1":
+        Console.WriteLine("Понедельник");
+        break;
+    case "2":
+        Console.WriteLine("Вторник");
+        break;
+    case "3":
+        Console.WriteLine("Среда");
+        break;
+    case "4":
+        Console.WriteLine("Четверг");
+        break;
+    case "5":
+        Console.WriteLine("Пятница");
+        break;
+    case "6":
+        Console.WriteLine("Суббота");
+        break;
+    case "7":
+        Console.WriteLine("Воскресенье");
+        break;
+    default:
+        Console.WriteLine("Ошибка ввода");
+        break;
+}
+
+
+
+
