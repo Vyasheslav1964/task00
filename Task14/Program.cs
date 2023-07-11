bcdf5287bdb1a3c1f533c37edea3cd5304d7befa
Vyasheslav1964 <137889188+Vyasheslav1964@users.noreply.github.com>
2023-07-11 17:48:32 +0300

    Added folder Task14

---
+++ Task14/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool result = IsMultiplicity(number);
+Console.WriteLine(result ? "да" : "нет");
+
+bool IsMultiplicity(int num)
+{
+    if (num % 7 == 0 && num % 23 == 0) return true;
+    return false;
+}
