ff743f3fed9dbaeb991d8828a59198be893a0725
Vyasheslav1964 <137889188+Vyasheslav1964@users.noreply.github.com>
2023-07-11 10:16:46 +0300

    Added folder Task11

---
+++ Task11/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+/*int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число диапазона 100 - 999 -> {number}");
+/*int firstDigit = number / 100;
+int secondDigit = number % 10;
+int resultDijit = firstDigit *10 + secondDigit;
+Console.WriteLine($"{number} -> {resultDijit}");*/
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число диапазона 100 - 999 -> {number}");
+int result = DeleteSecondDijit(number);
+Console.WriteLine($"{number} -> {result}");
+int DeleteSecondDijit (int num)
+{
+    int firstDigit = number / 100;
+    int secondDigit = number % 10;
+    int resultDijit = firstDigit * 10 + secondDigit;
+    return resultDijit;
+}
