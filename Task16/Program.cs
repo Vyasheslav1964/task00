015595d93e0e905a258520a2e8d57d9b4a807c07
Vyasheslav1964 <137889188+Vyasheslav1964@users.noreply.github.com>
2023-07-11 18:32:20 +0300

    Added folder Task16

---
+++ Task16/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Введите первое число");
+int numberFirst = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("ВВедите второе число");
+int numberSecond = Convert.ToInt32(Console.ReadLine());
+
+bool result = IsSquare(numberFirst, numberSecond);
+Console.WriteLine(result ? "да" : "нет");
+
+bool IsSquare (int num1, int num2)
+{
+  return num1 == num2 * num2 || num2 == num1*num1;
+}
