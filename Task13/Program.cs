58e1cae39b2821f7cf5ab5968da14dafe63f5074
Vyasheslav1964 <137889188+Vyasheslav1964@users.noreply.github.com>
2023-07-17 06:43:31 +0300

    Добавил задачи 13 и 15

---
+++ Task13/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int digit = num;
+// while (digit > 1000)
+// {
+// digit /= 10;
+// }
+// int thirdDigit = digit % 10;
+// Console.WriteLine($"{num} -> " + thirdDigit);
+
+int Prompt(string mess)
+{
+    Console.WriteLine(mess);
+    string value  = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int GetThirdDigital (int num)
+{
+    while (num > 999)
+    {
+        num /= 10;
+    }
+    return num % 10;
+}
+
+bool ValidateNum (int number)
+{
+    if (number < 100)
+    {
+        Console.WriteLine("третьей цифры нет");
+        return false;
+    }
+    return true;
+}
+
+int number = Prompt("Ведите число");
+if (ValidateNum(number))
+{
+    Console.WriteLine(GetThirdDigital (number));
+}
---
+++ Task15/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+int Prompt(string mess)
+{
+    Console.WriteLine(mess);
+    string value  = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+string WeekDay (int num)
+{
+    return num == 6 || num == 7 ? $" {num} -> да" :  $" {num} -> нет";
+}
+
+bool ErrorEnter(int num)
+{
+    if (1 > num || num > 7)
+    {
+        Console.WriteLine("Ошибка ввода ");
+        return false;
+    }
+    return true;
+
+}
+
+string message = "Введите номер дня недели";
+int numer = Prompt(message);
+//ErrorEnter(numer);
+if (ErrorEnter(numer)) Console.WriteLine(WeekDay(numer));
+
